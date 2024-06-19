0968c832ec74a60174e229430eaa1c918d9c255f
Rizalimaru <113398108+Rizalimaru@users.noreply.github.com>
2024-06-19 09:56:30 +0700

    Player Animation Done

--- UjiKompAgate2024/Assets/Script/PlayerController.cs
+++ UjiKompAgate2024/Assets/Script/PlayerController.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {   
     private CharacterController characterController;
+    private Animator animator;
     public float playerSpeed;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator =  GetComponent<Animator>();
     }
 
     void Update()
-    {
+    {   
         Vector3 move = new Vector3(0,0,Input.GetAxis("Horizontal"));
         characterController.Move(move * Time.deltaTime * playerSpeed);
 
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("LeftSrafe",true);
+        }else if(Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("LeftSrafe",false);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("RightSrafe",true);
+        }else if(Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("RightSrafe",false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Fire");
+        }
     }
 }
